8b5640d4d01a8a22f77aff1c5f1c5539a3af6447
Jagannath A <jagannath.sysop@gmail.com>
2019-04-02 16:53:54 +0530

    adds validation, alert messages and deleting

--- booklist/app.js
+++ booklist/app.js
@@ -6,21 +6,47 @@ function Book(title, author, isbn) {
 }
 
 // UI Constructor
-function UI() {
+function UI() {}
 
-  UI.prototype.addBookToList = function(book) {
-    const list = document.querySelector('#book-list');
-    // Create tr element
-    const row = document.createElement('tr');
-    // Insert cols
-    row.innerHTML = `
-    <td>${book.title}</td>
-    <td>${book.author}</td>
-    <td>${book.isbn}</td>
-    <td><a href="#" class="delete">X</a></td>
-    `;
+UI.prototype.addBookToList = function(book) {
+  const list = document.querySelector('#book-list');
+  // Create tr element
+  const row = document.createElement('tr');
+  // Insert cols
+  row.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.isbn}</td>
+  <td><a href="#" class="delete">X</a></td>
+  `;
 
-    list.appendChild(row);
+  list.appendChild(row);
+}
+
+// Show Alert
+UI.prototype.showAlert = function(message, className) {
+  // Create div
+  const div = document.createElement('div');
+  // Add classes
+  div.className = `alert ${className}`;
+  // Add text
+  div.appendChild(document.createTextNode(message));
+  // Get parent
+  const containerDiv = document.querySelector('.container');
+  // Get form
+  const form = document.querySelector('#book-form');
+  // Insert alert
+  containerDiv.insertBefore(div, form);
+  // Timeout after 3 seconds
+  setTimeout(function() {
+    document.querySelector('.alert').remove();
+  }, 3000);
+}
+
+// Delete book
+UI.prototype.deleteBook = function(target){
+  if(target.className === 'delete') {
+    target.parentElement.parentElement.remove();
   }
 }
 
@@ -32,7 +58,7 @@ UI.prototype.clearFields = function() {
 }
 
 
-// Event Listeners
+// Event Listener for adding the book
 document.querySelector('#book-form').addEventListener('submit', function(e) {
   // Get form values
   const title = document.querySelector('#title').value;
@@ -45,11 +71,33 @@ document.querySelector('#book-form').addEventListener('submit', function(e) {
   // Instantiate the UI
   const ui = new UI();
 
-  // Add book to list
-  ui.addBookToList(book);
-  
-  // Clear the fields
-  ui.clearFields();
+  // Validation
+  if(title === '' || author === '' || isbn === '') {
+    // Error alert message
+    ui.showAlert('Please fill in all fields', 'error');
+  } else {
+    // Add book to list
+    ui.addBookToList(book);
+    // Success alert message
+    ui.showAlert('Book added!', 'success');
+    // Clear the fields
+    ui.clearFields();
+  }
+
+  e.preventDefault();
+});
+
+// Event Listener for deleting the book
+document.querySelector('#book-list').addEventListener('click', function(e) {
+
+  // Instantiate the UI
+  const ui = new UI();
+
+  // Delete book
+  ui.deleteBook(e.target);
+
+  // Show message
+  ui.showAlert('Book removed!', 'success');
 
   e.preventDefault();
 });
