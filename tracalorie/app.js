7c34007e9bcbfb4ea36fde029df8e6121b4567fe
Jagannath A <jagannath.sysop@gmail.com>
2019-04-29 22:27:19 +0530

    adds functionality to add items to the data structure

--- tracalorie/app.js
+++ tracalorie/app.js
@@ -25,9 +25,33 @@ const ItemCtrl = (function() {
 
   // Public methods
   return {
+    getItems: function() {
+      return data.items;
+    },
+    addItem: function(name, calories) {
+      let ID;
+      // Create ID
+      if(data.items.length > 0) {
+        ID = data.items.length;
+        // ID = data.items[data.items.length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Calories to number
+      calories = parseInt(calories);
+
+      // Create new item
+      newItem = new Item(ID, name, calories);
+
+      // Add to items array
+      data.items.push(newItem);
+
+      return newItem;
+    },
     logData: function() {
       return data;
-    }
+    },
   }
 })();
 
@@ -37,11 +61,40 @@ const ItemCtrl = (function() {
 // UI Controller
 const UICtrl = (function() {
 
-  // Public methods
-  return {
-
+  const UISelectors = {
+    itemList: '#item-list',
+    addButton: '.add-btn',
+    itemNameInput: '#item-name',
+    itemCaloriesInput: '#item-calories',
   }
 
+  // Public methods
+  return {
+    populateItemList: function(items) {
+      let html = '';
+
+      items.forEach(function(item) {
+        html += `
+        <li class="collection-item" id="item-${item.id}">
+        <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+        <a href="#" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a>
+        </li>
+        `;
+      });
+
+      // Insert list items
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    },
+    getItemInput: function() {
+      return {
+        name: document.querySelector(UISelectors.itemNameInput).value,
+        calories: document.querySelector(UISelectors.itemCaloriesInput).value,
+      }
+    },
+    getSelectors: function() {
+      return UISelectors;
+    },
+  }
 })();
 
 
@@ -49,12 +102,41 @@ const UICtrl = (function() {
 
 // App Controller
 const App = (function(ItemCtrl, UICtrl) {
+  // Load event listeners
+  const loadEventListeners = function() {
+    // Get UI selectors
+    const UISelectors = UICtrl.getSelectors();
+
+    // Add item event
+    document.querySelector(UISelectors.addButton).addEventListener('click', itemAddSubmit);
+  }
+
+  // Add item submit
+  const itemAddSubmit = function(e) {
+    // Get form input from UI controller
+    const input = UICtrl.getItemInput();
+
+    // Check for name and calorie input
+    if(input.name !== '' && input.calories !== '') {
+      // Add item
+      const newItem = ItemCtrl.addItem(input.name, input.calories);
+    }
+
+    e.preventDefault();
+  }
 
   // Public methods
   return {
     init: function() {
-      console.log('Initialising App...');
-    }
+      // Fetch items from data structure
+      const items = ItemCtrl.getItems();
+
+      // populate list with items
+      UICtrl.populateItemList(items);
+
+      // Load event listeners
+      loadEventListeners();
+    },
   }
 
 })(ItemCtrl, UICtrl);
