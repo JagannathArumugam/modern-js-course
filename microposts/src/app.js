47a4c7d1bb0e3dd66421fa35c103cc4f40d050a9
Jagannath A <jagannath.sysop@gmail.com>
2019-05-18 23:54:15 +0530

    add functionality to add posts, show alerts and remove posts

--- microposts/src/app.js
+++ microposts/src/app.js
@@ -4,8 +4,50 @@ import { ui } from './ui';
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for submit post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+// Listen for delete post
+document.querySelector('#posts').addEventListener('click', deletePost);
+
 function getPosts() {
   http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
     .catch(err => console.log(err));
+}
+
+// Submit post
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title,
+    body
+  }
+
+  // Create post
+  http.post('http://localhost:3000/posts', data)
+    .then(data => {
+      ui.showAlert('Post Added', 'alert alert-success');
+      ui.clearFields();
+      getPosts();
+    })
+    .catch(err => console.log(err));
+}
+
+// Delete post
+function deletePost(e) {
+  if(e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id;
+    if(confirm('Are you sure?')) {
+      http.delete(`http://localhost:3000/posts/${id}`)
+        .then(data => {
+          ui.showAlert('Post Removed', 'alert alert-info');
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    }
+  }
+  e.preventDefault();
 }
